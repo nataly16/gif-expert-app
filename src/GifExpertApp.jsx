fa37bd5a8689728d5bbad5ef2bc46bb0c0ad007b
Nataly <portillonataly29@gmail.com>
2023-12-23 09:39:23 -0500

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const GifExpertApp = () => {
+  return (
+    <>
+        {/* titulo */}
+        <h1>GifEpertApp</h1>
+
+        {/* Input */}
+
+        {/* Listado de Gif */}
+            {/* Git Item */}
+    </>
+  )
+}
